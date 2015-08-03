963c3e63f7c503bfe8f099ccd46efcb1b5fc18f7
cexoso <285516193@qq.com>
2015-08-03 17:58:13 +0800

    nd

--- static/app.js
+++ static/app.js
@@ -11,9 +11,28 @@ $(function(){
     $('#displayBoard').on('slideDown.custom',function(){
         console.log('slideDown');
     })
-    $("#loading").hide();
+
+
+    for(var i=0;i<10;i++){
+        (function(i){
+            setTimeout(function(){
+                $('#loading').css({opacity:1-i*0.1});
+            }, i*50);    
+        })(i);
+        setTimeout(function(){
+                $('#loading').hide();
+        }, 500);    
+    }
+    
    
     $('#page2 .banner').on('click',function(){
         console.log($(this))
+    })
+
+    $('#shareBtn').on('click',function(){
+        $('#share').show();
+        $('#share').one('click',function(){
+            $(this).hide();
+        });
     })
 })
